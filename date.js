e13594a1e2cd0b6b01f091856f9a719ad09f9ebb
Vinit Pundeer <79745487+Vinit1236@users.noreply.github.com>
2023-05-25 15:28:42 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,15 +1,11 @@
 //jshint esversion:6
 
 exports.getDate = function() {
-
-  const today = new Date();
-
+  const today=new Date();
   const options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-
   return today.toLocaleDateString("en-US", options);
-
 };
